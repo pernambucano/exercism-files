ff71e2e07cd507cf5827d239e8ae27e7eab1e2b1
Paulo <demelofernandes.paulo@gmail.com>
2020-02-27 23:47:57 -0300

    difference-of-squares

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,18 +1,10 @@
 def square_of_sum(count):
-    sum = 0
-    for number in range(count+1):
-        sum += number
-    return sum**2 
+    return sum(range(1, count + 1)) ** 2
+
 
 def sum_of_squares(count):
-    sum = 0
-    for number in range(count+1):
-        sum += number**2
-    return sum
-    
+    return sum(i ** 2 for i in range(1, count + 1))
 
 
 def difference(count):
-    sqsum = square_of_sum(count)
-    sumsq = sum_of_squares(count)
-    return sqsum - sumsq
+    return square_of_sum(count) - sum_of_squares(count)
