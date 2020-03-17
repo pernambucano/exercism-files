f518e5ff7631d688e7ccbddce66cc197a07e04bb
Paulo <demelofernandes.paulo@gmail.com>
2020-03-16 23:15:50 -0300

    clean code

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,47 +1,46 @@
 import threading
 
-LOCK = threading.Lock()
-
 
 class BankAccount:
     def __init__(self):
         self.balance = 0
         self.is_open = False
+        self.lock = threading.Lock()
+
+    def validate_if_open(self):
+        if self.is_open == False:
+            raise ValueError("Account not opened.")
 
     def get_balance(self):
-        if self.is_open == False:
-            raise ValueError("Can not get the current balance of a closed account.")
-        with LOCK:
+        with self.lock:
+            self.validate_if_open()
             return self.balance
 
     def open(self):
-        if self.is_open == True:
-            raise ValueError("Can not open an already opened account.")
-
-        with LOCK:
+        with self.lock:
+            if self.is_open == True:
+                raise ValueError("Already opened.")
             self.is_open = True
             self.balance = 0
 
     def deposit(self, amount):
-        if amount < 0:
-            raise ValueError("Can not deposit negative values.")
-        elif self.is_open == False:
-            raise ValueError("Can not deposit into closed accounts.")
-
-        with LOCK:
+        with self.lock:
+            if amount < 0:
+                raise ValueError("Can not deposit negative values.")
+            self.validate_if_open()
             self.balance += amount
 
     def withdraw(self, amount):
-        if amount > self.balance:
-            raise ValueError("Can not withdraw more than the current balance.")
-        elif amount < 0:
-            raise ValueError("Can not withdraw negative numbers.")
-
-        with LOCK:
+        with self.lock:
+            self.validate_if_open()
+            if amount > self.balance:
+                raise ValueError("Can not withdraw more than the current balance.")
+            elif amount < 0:
+                raise ValueError("Can not withdraw negative numbers.")
             self.balance -= amount
 
     def close(self):
-        if self.is_open == False:
-            raise ValueError("Can not close an already closed account.")
-        with LOCK:
+        with self.lock:
+            if self.is_open == False:
+                raise ValueError("Account already closed.")
             self.is_open = False
